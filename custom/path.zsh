93c41cdf592f018f5eee3c0821bfc2ae6bf1b15f
Anton Hedström <antonhedstrom@gmail.com>
2018-03-12 18:47:03 +0100

    Added Flutter and commented out Heroku.

--- custom/path.zsh
+++ custom/path.zsh
@@ -1,6 +1,10 @@
 
 ### Added by the Heroku Toolbelt
-export PATH="$PATH:/usr/local/heroku/bin"
+# export PATH="$PATH:/usr/local/heroku/bin"
 
 # Mysql
 export PATH="$PATH:/usr/local/mysql/bin"
+
+# Flutter
+export PATH="$PATH:/Users/antonhedstrom/Applications/flutter/bin"
+
