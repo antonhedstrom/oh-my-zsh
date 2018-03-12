1cd61305adea29a2197e66f2b03af53c9b5de8ec
Anton Hedström <antonhedstrom@gmail.com>
2018-03-12 18:46:48 +0100

    Fixed brew issues.

--- custom/exports.zsh
+++ custom/exports.zsh
@@ -1,5 +1,2 @@
 # Don’t clear the screen after quitting a manual page
 export MANPAGER="less -X"
-
-# http://stackoverflow.com/questions/17610417/permission-denied-library-caches-homebrew-formula-nmap-brewing
-export HOMEBREW_CACHE="~/Library/Caches/Homebrew"
